f595d28a2be4c06e815681459587e7537c10727a
Richard J. de Borja <richard.deborja@uhnresearch.ca>
2017-08-17 17:23:25 -0400

    fixed usage for all functions, added merge function

--- core/general.py
+++ core/general.py
@@ -23,9 +23,23 @@ def get_sample_name_from_fastq_file(file=None):
     OUTPUT:
         Returns a string with the sample name extrated.
     """
-    # to extract the sample name, we need to remove the following from the:
+    # to extract the sample name, we need to remove the following:
     #   * _L00[1-8]_R[12][0-9_]*\.fastq\.gz
-    return re.sub("[L0-8_]*_R[12][0-9_]*\.fastq\.gz", "", file)
+    #   * _R[12]_[0-9]*\.fastq.gz
+    #.  * _L00[1-8]_R[12]\.fastq.gz
+    if re.search("_L[0-8]*_R[12][0-9_]*\.fastq\.gz", file):
+        sample = re.sub("_L[0-8_]*_R[12][0-9_]*\.fastq\.gz", "", file)
+    elif re.search("_R[12]_[0-9]*\.fastq\.gz", file):
+        sample = re.sub("_R[12]_[0-9]*\.fastq\.gz", "", file)
+    elif re.search("_L[0-8]*_R[12]\.fastq\.gz", file):
+        sample = re.sub("_L[0-8]*_R[12]\.fastq\.gz", "", file)
+    elif re.search("_R[12]\.fastq\.gz", file):
+        sample = re.sub("_R[12]\.fastq\.gz", "", file)
+    else:
+        print("File " + file + " is not a paired-end FASTQ file")
+        sys.exit()
+    return sample
+
 
 def get_paired_fastq_files(dir=None):
     """
@@ -34,9 +48,7 @@ def get_paired_fastq_files(dir=None):
     representing the corresponding value.
 
     USAGE:
-        object.get_paired_fastq_files(
-            dir
-            )
+        get_paired_fastq_files(dir)
 
     INPUT:
         * dir: the directory containing the FASTQ files
@@ -52,30 +64,59 @@ def get_paired_fastq_files(dir=None):
 
     paired_fastq_files = {}
     for _fastq_file in fastq_files:
-        _samplename = get_sample_name_from_fastq_file(file=_fastq_file)
-        if _samplename in paired_fastq_files.keys():
-            if re.search("_R1\.", _fastq_file):
-                _read2 = re.sub("_R1.", "_R2.", _fastq_file)
-                paired_fastq_files[_sample_name][_fastq_file] = _read2
-            elif re.search("_R1_", _fastq_file):
-                _read2 = re.sub("_R1_", "_R2_", _fastq_file)
-                paired_fastq_files[_samplename][_fastq_file] = _read2
-
+        if re.search("\.fastq.gz$", _fastq_file):
+            _samplename = get_sample_name_from_fastq_file(file=_fastq_file)
+            if _samplename in paired_fastq_files.keys():
+                if re.search("_R1\.", _fastq_file):
+                    _read2 = re.sub("_R1.", "_R2.", _fastq_file)
+                    paired_fastq_files[_samplename][_fastq_file] = _read2
+                elif re.search("_R1_", _fastq_file):
+                    _read2 = re.sub("_R1_", "_R2_", _fastq_file)
+                    paired_fastq_files[_samplename][_fastq_file] = _read2
+            else:
+                if re.search("_R1\.", _fastq_file):
+                    _read2 = re.sub("_R1.", "_R2.", _fastq_file)
+                    paired_fastq_files[_samplename] = {_fastq_file: _read2}
+                elif re.search("_R1_", _fastq_file):
+                    _read2 = re.sub("_R1_", "_R2_", _fastq_file)
+                    paired_fastq_files[_samplename] = {_fastq_file: _read2}
+        else:
+            continue
     return paired_fastq_files
 
 
-def merge_fastq_files(dir=None):
+def merge_fastq_files(dir='.'):
     """
     Merge multiple read 1 and read 2 FASTQ files into a single read 1 FASTQ
     file and a single read 2 FASTQ file.
 
     USAGE:
-        object.merge_fastq_files(dir)
+        merge_fastq_files(dir)
 
     INPUT:
-        * dir: full path to the directory containing FASTQ files.
+        * dir: full path to the directory containing FASTQ files
 
     OUTPUT:
-        list of return values
+        Creates a directory based on the sample name and constructs the command
+        to concatenate files of the same sample for read1 and read2.
     """
-    
+    read1 = []
+    read2 = []
+    _fastq_files = get_paired_fastq_files(dir=dir)
+    if len(_fastq_files) == 0:
+        print("No FASTQ files found...")
+        sys.exit()
+    else:
+        for sample in _fastq_files.keys():
+            os.makedirs(sample)
+            for _fastq_read1 in _fastq_files[sample].keys():
+                read1.append(_fastq_read1)
+                read2.append(_fastq_files[sample][_fastq_read1])
+            read1_files = ' '.join(read1)
+            read2_files = ' '.join(read2)
+            read1_merged_file = ''.join([sample, '_R1.fastq.gz'])
+            read2_merged_file = ''.join([sample, '_R2.fastq.gz'])
+            cmd_read1 = ' '.join(["cat", read1_files, '>', '/'.join([sample, read1_merged_file])]) 
+            cmd_read2 = ' '.join(["cat", read2_files, '>', '/'.join([sample, read2_merged_file])])
+            return_val = {sample: {"cmd_read1":cmd_read1, "cmd_read2":cmd_read2}}
+    return return_val
